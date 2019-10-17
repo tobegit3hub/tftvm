ae294b48a3542050bf98e67bb9bb42990ba0f385
tobe <tobeg3oogle@gmail.com>
2019-10-17 14:44:50 +0800

    Update test script for tvm runtime op

--- tvm_runtime_op/test_tvm_runtime_op.py
+++ tvm_runtime_op/test_tvm_runtime_op.py
@@ -4,19 +4,11 @@ import tensorflow as tf
 from tensorflow.python.framework import load_library
 from tensorflow.python.platform import resource_loader
 
-_zero_out_ops = load_library.load_op_library(
-            #resource_loader.get_path_to_datafile('_zero_out_ops.so'))
-            #@resource_loader.get_path_to_datafile('zero_out.so'))
+_tvm_runtime_ops = load_library.load_op_library(
             resource_loader.get_path_to_datafile('tvm_runtime.so'))
-            #resource_loader.get_path_to_datafile('./tensorflow_zero_out/python/ops/_zero_out_ops.so'))
-zero_out = _zero_out_ops.zero_out
-
-#import ipdb;ipdb.set_trace()
-
-t = zero_out(tf.constant([10, 20, 11, -30]))
+tvm_runtime = _tvm_runtime_ops.tvm_runtime
 
 sess = tf.Session()
 
-t_value = sess.run(t)
-print(t_value)
-
+t = tvm_runtime(tf.constant([10, 20, 11, -30]))
+print(sess.run(t))
