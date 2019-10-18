a34186aa20183b4264e67469db685aad4ed6b5ec
tobe <tobeg3oogle@gmail.com>
2019-10-18 12:26:44 +0800

    Wrap tesnroflow custom op as python library

--- tvm_runtime_op/test_tvm_runtime_op.py
+++ tvm_runtime_op/test_tvm_runtime_op.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
 import tensorflow as tf
-from tensorflow.python.framework import load_library
-from tensorflow.python.platform import resource_loader
 
-_tvm_runtime_ops = load_library.load_op_library(
-            resource_loader.get_path_to_datafile('tvm_runtime.so'))
-tvm_runtime = _tvm_runtime_ops.tvm_runtime
+from tvm_runtime_op import tvm_runtime
 
 with tf.Session() as sess:
   output = tvm_runtime(tf.constant([10, 20, 11, -30]))
---
+++ tvm_runtime_op/tvm_runtime_op.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+from tensorflow.python.framework import load_library
+from tensorflow.python.platform import resource_loader
+
+_tvm_runtime_ops = load_library.load_op_library(
+            resource_loader.get_path_to_datafile('tvm_runtime.so'))
+tvm_runtime = _tvm_runtime_ops.tvm_runtime
+
