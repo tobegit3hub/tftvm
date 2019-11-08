a31cf7f48e8ac2fd31b2d308bdd03459b8decd4c
tobe <tobeg3oogle@gmail.com>
2019-11-08 15:14:12 +0800

    Remove old kernel op and python library

--- tvm_runtime_op/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
-TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
-
-g++ -g -std=c++11 -shared tvm_runtime_kernels.cc tvm_runtime_ops.cc -o tvm_runtime_op.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 -I${TVM_HOME}/include -I${TVM_HOME}/3rdparty/dmlc-core/include -I${TVM_HOME}/3rdparty/dlpack/include -ltvm_runtime -L${TVM_HOME}/build -ldl -lpthread 
--- tvm_runtime_op/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-export LD_LIBRARY_PATH=${TVM_HOME}/build/:${LD_LIBRARY_PATH}
-./test_tvm_runtime_op.py
